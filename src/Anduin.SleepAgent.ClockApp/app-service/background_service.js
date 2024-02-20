1672e1b59c8668fd11ab2986bbb2fd7efe28fb1e
AnduinXue <anduin@aiursoft.com>
2024-02-20 12:19:18 +0000

    Expand data collection in background service
    The changes made in the background service script include the addition of more sensor data collection and a change in API endpoint. The application will now collect various health metrics such as heart rate, battery level, blood oxygen level, calories, distance traveled, among other variables. This is achieved by using additional methods from the sensor library.

--- src/Anduin.SleepAgent.ClockApp/app-service/background_service.js
+++ src/Anduin.SleepAgent.ClockApp/app-service/background_service.js
@@ -1,28 +1,68 @@
 import * as notificationMgr from "@zos/notification";
 import { Time } from '@zos/sensor'
-import { Sleep } from '@zos/sensor';
 import { BasePage } from "@zeppos/zml/base-page";
+import { HeartRate, Battery, BloodOxygen, Calorie, Distance, FatBurning, Pai, Sleep, Stand, Step, Stress, Wear } from "@zos/sensor";
+import { getProfile } from '@zos/user'
+import { getDeviceInfo } from '@zos/device'
 
 const timeSensor = new Time();
-const debugging = true;
+const debugging = false;
+const endPoint = "http://lab:12222/api/metric"
 
 // Send a notification
 function sendNotification(vm) {
 
+  const deviceInfo = getDeviceInfo();
+  const heartRate = new HeartRate();
+  const battery = new Battery();
+  const bloodOxygen = new BloodOxygen();
+  const calorie = new Calorie();
+  const distance = new Distance();
+  const fatBurning = new FatBurning();
+  const pai = new Pai();
   const sleep = new Sleep();
+  const stand = new Stand();
+  const step = new Step();
+  const stress = new Stress();
+  const wear = new Wear();
+
   sleep.updateInfo();
-  const info = sleep.getInfo();
-  const score = info.score;
-  const isCurrentlySleeping = sleep.getSleepingStatus(); // 0 醒着，1 正在睡眠
 
   const reqBody = {
-    score: score,
-    isCurrentlySleeping: isCurrentlySleeping
+    usrProf: getProfile(),
+    device: deviceInfo,
+    hr: heartRate.getLast(),
+    hrRes: heartRate.getResting(),
+    hrSum: heartRate.getDailySummary(),
+    bat: battery.getCurrent(),
+    spo2: bloodOxygen.getCurrent(),
+    spo2Hr: bloodOxygen.getLastFewHour(),
+    calC: calorie.getCurrent(),
+    calT: calorie.getTarget(),
+    dist: distance.getCurrent(),
+    fatBC: fatBurning.getCurrent(),
+    fatBT: fatBurning.getTarget(),
+    paiDay: pai.getToday(),
+    paiWeek: pai.getTotal(),
+    sleepInfo: sleep.getInfo(),
+    sleepStages: sleep.getStage(),
+    sleepStgList: sleep.getStageConstantObj(),
+    sleepCurrent: sleep.getSleepingStatus(),
+    standC: stand.getCurrent(),
+    standT: stand.getTarget(),
+    stepC: step.getCurrent(),
+    stepT: step.getTarget(),
+    stress: stress.getCurrent(),
+    wear: wear.getStatus(),
   };
+
   vm.httpRequest({
     method: 'POST',
-    url: 'https://www.aiursoft.cn/api/metrics/',
+    url: endPoint,
     body: JSON.stringify(reqBody),
+    headers: {
+      'Content-Type': 'application/json'
+    }
   })
   .then((result) => {
     const status = result.status;
